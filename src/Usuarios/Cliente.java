980d331ae20a578bf173faebbc158d60cad2b689
Ar1sAlan1s <Arisalanis468@gmail.com>
2024-05-09 09:32:21 -0600

    jejejejej

--- src/Usuarios/Cliente.java
+++ src/Usuarios/Cliente.java
@@ -1,6 +1,7 @@
 package Usuarios;
 
 import Tarjetas.Credito;
+import Tarjetas.Debito;
 import Usuarios.Utils.Rol;
 import Usuarios.Utils.Sucursales;
 import Tarjetas.Tarjeta;
@@ -9,11 +10,13 @@ import java.util.HashMap;
 
 public class Cliente extends Usuario {
     private LocalDate fechaDeRegistro;
+    Debito debito;
     Rol rol;
     public HashMap<Long, Credito>tarjetasCredito;
-    public Cliente(String usuario, String password, String nombre, String apellidos, LocalDate fechaNacimiento, String ciudad, String estado, String RFC, String Curp, String direccion, Sucursales sucursales, Rol rol, Tarjeta debito, HashMap tarjetasCredito) {
+    public Cliente(String usuario, String password, String nombre, String apellidos, LocalDate fechaNacimiento, String ciudad, String estado, String RFC, String Curp, String direccion, Sucursales sucursales, Rol rol, Debito debito, HashMap tarjetasCredito) {
         super(usuario, password, nombre, apellidos, fechaNacimiento, ciudad, estado , RFC, Curp, direccion, sucursales, Rol.Cliente);//Asigno de una vez el Rol Cliente
         this.rol=rol;
         this.tarjetasCredito=tarjetasCredito;
+        this.debito=debito;
     }
 }
